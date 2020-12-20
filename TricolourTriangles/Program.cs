8c218f6743252755a30c76282025f54b92317a9f
Kevin de Haan <kbehaan@gmail.com>
2020-12-20 13:42:01 -0700

    converted from .net core to .net framework

---
+++ TricolourTriangles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+
+namespace TricolourTriangles
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class ColourTriangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a perimeter length (must be >= 3):");
+            //while(true)
+            //{
+            string input = Console.ReadLine();
+            try
+            {
+                PolygonGraph polygon = new PolygonGraph(int.Parse(input));
+            }
+            catch (Exception e) when (e is ArgumentException || e is FormatException)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //}
+
+        }
+    }
+}
