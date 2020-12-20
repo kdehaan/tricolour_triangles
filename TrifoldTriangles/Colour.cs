4767624db7cf25c5a21d2c89726291cace1f9b33
Kevin de Haan <kbehaan@gmail.com>
2020-12-20 12:40:21 -0700

    added unit testing

---
+++ TrifoldTriangles/Colour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TricolourTriangles
+{
+    public enum Colour
+    {
+        Red, 
+        Green,
+        Blue
+    }
+}
---
+++ TrifoldTriangles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TricolourTriangles
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class ColourTriangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Polygon polygon = new Polygon(5);
+        }
+    }
+}
